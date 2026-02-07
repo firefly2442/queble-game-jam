8d2a86dcc3ba11427edd4baa2459a376e5f85eeb
firefly2442 <firefly2442@gmail.com>
2026-02-07 14:08:12 -0600

    feat: add falling snowflakes

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+var image_list: Array[String] = ["uid://bv43g3rcv4jwj", "uid://ceq4p6wt3xybu"]
+var image_index: int = 0
+
 func _ready() -> void:
-	pass
+	(%ImageTimer as Timer).start()
 
 func _on_exit_button_pressed() -> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
@@ -25,3 +28,21 @@ func _toggle_fullscreen() -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+func _show_next_image() -> void:
+	var tex_rect: TextureRect = %MenuTextureRect
+	var next_index: int = (image_index + 1) % image_list.size()
+	# Fade out
+	var tween: Tween = create_tween()
+	tween.tween_property(tex_rect, "modulate:a", 0.0, 0.5).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_callback(func() -> void:
+		tex_rect.texture = load(image_list[next_index])
+	)
+	tween.tween_property(tex_rect, "modulate:a", 1.0, 0.5).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+
+	image_index = next_index
+
+
+func _on_image_timer_timeout() -> void:
+	_show_next_image()
+	(%ImageTimer as Timer).start()
